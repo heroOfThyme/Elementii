13fbf2f5574c651d1b4f1ea1296550096ab810ee
heroOfThyme <p.vidako@gmail.com>
2025-02-25 20:51:08 -0500

    Initial Commit

---
+++ Elementii/ElementiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElementiiApp.swift
+//  Elementii
+//
+//  Created by Petar Vidakovic on 2025-02-25.
+//
+
+import SwiftUI
+
+@main
+struct ElementiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ElementiiTests/ElementiiTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElementiiTests.swift
+//  ElementiiTests
+//
+//  Created by Petar Vidakovic on 2025-02-25.
+//
+
+import Testing
+@testable import Elementii
+
+struct ElementiiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
